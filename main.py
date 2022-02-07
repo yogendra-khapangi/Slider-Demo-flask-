05fd84979e7370689bdcc3bee1a0fba096191706
hari <youyogendra@gmail.com>
2022-02-07 20:57:31 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+@app.route("/sider")
+def slider():
+    return render_template("basic.html")
+
+@app.route("/analytic")
+def analytic():
+    return render_template("Analytics.html")
+
+
+
+
+
+app.run(debug=True)
+
+
